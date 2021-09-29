57b2843810be17b949756c80083049539a76476c
mz-rizkii <rizki-rzk2@gmail.com>
2021-09-29 22:16:05 +0700

    Refactor find string in bracket

--- test/unit/test_refactor_function.js
+++ test/unit/test_refactor_function.js
@@ -4,18 +4,25 @@ const { findFirstStringInBracket, findFirstStringInBracketOld } = require('../..
 
 it('check bracket pairing', checkBracketPairing);
 
+/**
+ * Test refactor function ensure refactor function produce same result as the old one
+ * old version of findFirstStringInBracket renamed to findFirstStringInBracketOld
+ */
 function checkBracketPairing() {
-    const demo_input = "(TnT)";
+    const demo_inputs = [
+        ["(TnT)", 'TnT'], ["kunci()", ''], ["Internet of things(IoT)", 'IoT'],
+        ["Bantuan", ''], ['<):I:(>', '']
+    ];
 
-    const expected_result = 'TnT';
+    for (const [demo_input, expected_result] of demo_inputs) {
+        const result = findFirstStringInBracket(demo_input);
 
-    const result = findFirstStringInBracket(demo_input);
+        expect(result, 'the result should be string inside bracket').to.equal(expected_result)
 
-    expect(result, 'the result should be string inside bracket').to.equal(expected_result)
+        const old_result = findFirstStringInBracketOld(demo_input);
 
-    const old_result = findFirstStringInBracketOld(demo_input);
+        expect(result, 'the result should be string inside bracket').to.equal(expected_result);
 
-    expect(result, 'the result should be string inside bracket').to.equal(expected_result);
-
-    expect(result, 'the result from both function should be same').to.equal(old_result);
+        expect(result, 'the result from both function should be same').to.equal(old_result);
+    }
 }
