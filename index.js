0d4dc85e51103ee2a568aced061e49c4fcd0ced2
mz-rizkii <rizki-rzk2@gmail.com>
2021-10-03 13:45:24 +0700

    Read port from env

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const express = require('express')
 
 const router = require('./lib/router');
 
+const { app_port } = require('./lib/helper/config_helper');
+
 const app = express()
-const port = 3000
 
 app.use('/', router)
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.listen(app_port, () => {
+  console.log(`Example app listening at http://localhost:${app_port}`)
 });
 
 module.exports = app;
